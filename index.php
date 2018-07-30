04d6546e3fb400468f23479f255f4781bf3c4796
Greg Morgan <gregorytmorgan@gmail.com>
2018-07-30 15:53:37 -0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Demos</title>
+  </head>
+  <body>
+    <h1>Demos</h1>
+    <ul>
+      <li><a href="jquery-custom-widgets/index.html">jQuery widget demo</a></li>
+      <li><a href="scratch/index.php">scratch-html</a></li>
+      <li><a href="nps-online-res-test-site/index.html">nps-online-res Test Site</a></li>
+      <li><a href="http://127.0.0.1:8000">Django demo</a> (Django test server must be running @ 127.0.0.1:8000 Use .../mysite/.runserver)</li>
+      <li><a href="http://127.0.0.1:4200">Ember demo</a> (Ember test server must be running @ 127.0.0.1:4200 Use .../super-rentals/ember serve)</li>
+      <li><a href="btree-draw.php">BTree Draw</a></li>
+      <li><a href="quicken-report-parser/index.php">Quicken Report Parser</a></li>
+    </ul>
+   <h1>Utils</h1>
+    <ul>
+      <li><a href="phpinfo.php">PHP Info</a></li>
+    </ul>   
+  </body>
+</html>
+
